09d3de96b2dd18fcde11b5e1c934a6e0db14eb81
nadia winy <nadiawiny@hotmail.com>
2023-10-03 12:46:56 -0300

    ..

---
+++ aula -03/beyond-app/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-app> <!--tudo que tiver dentro dele vai funcionar com vuetify "PAI"-->
+    <v-main>
+      <v-app-bar color = 'green'>Maria</v-app-bar> <!--toolbar-->
+        <div v-for="(message, index) in messages" :key="index">
+          <message-card :messageProp="message"/>
+        </div>
+      <v-app-bar fixed bottom color = '#424242'><!--bottom-bar-->
+        <v-text-field v-model="textInput" @keyup.enter="sendMessage()" solo hide-details label="Enviar mensagem..."></v-text-field>
+        <v-btn @click="sendMessage()" icon color="blue">
+          <v-icon>mdi-send</v-icon>
+        </v-btn>
+      </v-app-bar> 
+    </v-main>
+  </v-app>
+  </template>
+  
+  <script>
+    import MessageCard from './components/MessageCard.vue'
+    export default{
+      components: {
+        MessageCard
+      },
+      data(){ // onde vai ta as variaveis da nossa aplicação
+        return{
+          textInput: '',
+          messages: [
+            {
+              name: 'Maria',
+              text: 'olá'
+            },
+            {
+              name: 'Maria',
+              text: 'tudo bem?'
+            }
+          ]
+        }
+      },
+      methods: {
+        sendMessage(){
+          let message = {
+            name: "Eu",
+            text: this.textInput
+          }
+          this.messages.push(message)
+          this.textInput = ''
+        }
+      }
+      
+    }
+  </script>
+  
---
+++ aula -03/beyond-app/src/components/MessageCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-card :class="messageProp.name != 'Eu' ? 'teal' : 'purple'" outlined class="ma-3 accent-4">
+     <v-card-subtitle class="front-weight-bold">{{ messageProp.name }}</v-card-subtitle>
+     <v-card-text>{{ messageProp.text }}</v-card-text>
+   </v-card> 
+ </template>
+ 
+<script>
+export default {
+    props: {
+        messageProp: {
+            type: Object,
+            required: true
+        }
+        
+    }
+
+
+
+}
+</script>
