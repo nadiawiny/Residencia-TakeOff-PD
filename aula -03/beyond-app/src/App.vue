3e2407d9d8d600fae748240284e111f0492efe12
nadia winy <nadiawiny@hotmail.com>
2023-10-04 17:16:28 -0300

    chat-componests

--- aula -03/beyond-app/src/App.vue
+++ aula -03/beyond-app/src/App.vue
@@ -5,25 +5,23 @@
         <div v-for="(message, index) in messages" :key="index">
           <message-card :messageProp="message"/>
         </div>
-      <v-app-bar fixed bottom color = '#424242'><!--bottom-bar-->
-        <v-text-field v-model="textInput" @keyup.enter="sendMessage()" solo hide-details label="Enviar mensagem..."></v-text-field>
-        <v-btn @click="sendMessage()" icon color="blue">
-          <v-icon>mdi-send</v-icon>
-        </v-btn>
-      </v-app-bar> 
+        <BottomBar @send-message = "addMassage($event)" />
     </v-main>
   </v-app>
   </template>
   
   <script>
     import MessageCard from './components/MessageCard.vue'
+    import BottomBar from './components/BottomBar.vue'
+
     export default{
       components: {
-        MessageCard
+        MessageCard,
+        BottomBar
       },
       data(){ // onde vai ta as variaveis da nossa aplicação
         return{
-          textInput: '',
+  
           messages: [
             {
               name: 'Maria',
@@ -37,13 +35,8 @@
         }
       },
       methods: {
-        sendMessage(){
-          let message = {
-            name: "Eu",
-            text: this.textInput
-          }
-          this.messages.push(message)
-          this.textInput = ''
+        addMassage(e){
+          this.messages.push(e)
         }
       }
       
---
+++ aula -03/beyond-app/src/components/BottomBar.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-app-bar fixed bottom color = '#424242'><!--bottom-bar-->
+        <v-text-field v-model="textInput" @keyup.enter="sendMessage()" solo hide-details label="Enviar mensagem..."></v-text-field>
+        <v-btn @click="sendMessage()" icon color="blue">
+          <v-icon>mdi-send</v-icon>
+        </v-btn>
+    </v-app-bar> 
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                textInput: '',
+            }
+        },
+        methods: {
+            sendMessage(){
+                let message = {
+                    name: "Eu",
+                    text: this.textInput
+                }
+                this.$emit('send-message', message)
+                this.textInput = '' // para limpar depois de digitar
+            }
+        }
+    }
+</script>
